d060b1333cb5b6fa23fa47008f827ce33a470a16
Birhan121994 <birhantamiru281@gmail.com>
2025-08-26 21:20:18 +0100

    Auto-create superuser on deploy

--- backend/build.sh
+++ backend/build.sh
@@ -20,3 +20,14 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
+
+python manage.py shell << END
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+if password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+END
